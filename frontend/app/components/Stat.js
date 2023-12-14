fa2fd0681536b6e1a938d24eda8ba70442a66c1b
Luke Tang <tanlu824@student.otago.ac.nz>
2023-12-15 10:31:11 +1300

    Format x-axis names for weapon stat chart

--- frontend/app/components/Stat.js
+++ frontend/app/components/Stat.js
@@ -1,42 +1,10 @@
 "use client";
 
 export default function Stat(props) {
-    function convertFromCamelCase(string) {
-        return string
-        // insert a space before all caps
-        .replace(/([A-Z])/g, " $1")
-        // uppercase the first character
-        .replace(/^./, function (str) {
-            return str.toUpperCase();
-        });
-    }
-
-    function formatName(name) {
-        return props.sectionStatNames.get(name);
-    }
-
-    function formatValue(displayValue) {
-        // Show time played in hours
-        if (props.name === "secondsPlayed") {         
-            return Math.floor(props.value / 3600) + " hours";
-        }
-
-        // Format numerical values
-        const parsedValue = +displayValue;
-        if (!isNaN(parsedValue)) {
-            return parsedValue.toLocaleString(undefined, {maximumFractionDigits:2});
-        }
-
-        return displayValue;
-    }
-
-    const formattedName = formatName(props.name);
-    const formattedDisplayValue = formatValue(props.displayValue);
-
     return (
         <div className="stat">
-            <h4>{formattedName}</h4>
-            {formattedDisplayValue}
+            <h4>{props.formattedName}</h4>
+            {props.formattedDisplayValue}
         </div>
     );
 }
--- frontend/app/components/StatSection.js
+++ frontend/app/components/StatSection.js
@@ -15,6 +15,27 @@ import { useState } from "react";
 export function StatSection(props) {
     const [viewAsGraph, setViewAsGraph] = useState(false);
 
+    function formatName(name) {
+        return props.sectionStatNames.get(name);
+    }
+
+    function formatValue(displayValue, isSecondsPlayed = false) {
+        // Show time played in hours
+        if (isSecondsPlayed) {
+            return Math.floor(displayValue / 3600) + " hours";
+        }
+
+        // Format numerical values
+        const parsedValue = +displayValue;
+        if (!isNaN(parsedValue)) {
+            return parsedValue.toLocaleString(undefined, {
+                maximumFractionDigits: 2,
+            });
+        }
+
+        return displayValue;
+    }
+
     let hasUndefined = true;
     const statList = Array.from(props.sectionStatNames.keys()).map((stat) => {
         // Only show stats that are known
@@ -23,13 +44,22 @@ export function StatSection(props) {
             props.stats[stat] !== undefined
         ) {
             hasUndefined = false; // as there is at least one stat in this section
+
+            const formattedName = formatName(stat);
+
+            let formattedDisplayValue = undefined;
+            if (stat === "secondsPlayed") {
+                formattedDisplayValue = formatValue(props.stats[stat].basic.value, true);
+            } else {
+                formattedDisplayValue = formatValue(props.stats[stat].basic.displayValue);
+            }
+
             return (
                 <Stat
                     key={stat}
                     sectionStatNames={props.sectionStatNames}
-                    name={stat}
-                    value={props.stats[stat].basic.value}
-                    displayValue={props.stats[stat].basic.displayValue}
+                    formattedName={formattedName}
+                    formattedDisplayValue={formattedDisplayValue}
                 />
             );
         } else {
@@ -48,7 +78,7 @@ export function StatSection(props) {
                 ) {
                     hasUndefined = false; // as there is at least one stat in this section
                     return {
-                        name: stat,
+                        name: formatName(stat),
                         value: props.stats[stat].basic.value,
                     };
                 } else {
@@ -76,9 +106,9 @@ export function StatSection(props) {
                 {viewAsGraph === false ? (
                     <div className="stat-section">{statList}</div>
                 ) : (
-                    <ResponsiveContainer minWidth={400} minHeight={400}>
+                    <ResponsiveContainer width="100%" minHeight={400}>
                         <BarChart data={weaponGraphData}>
-                            <XAxis dataKey="name" />
+                            <XAxis dataKey="name" interval={0} angle={-30} textAnchor="end" minTickGap={1000} height={90} />
                             <YAxis />
                             <Tooltip />
                             <Bar dataKey="value" fill="#82ca9d" />
