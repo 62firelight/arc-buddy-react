9b85435d15a87ad517f6126c47d7579d400ffcc0
Luke Tang <tanlu824@student.otago.ac.nz>
2023-12-14 13:01:35 +1300

    Add bar chart for weapon stats (albeit unformatted)

--- frontend/app/components/StatSection.js
+++ frontend/app/components/StatSection.js
@@ -1,14 +1,27 @@
 "use client";
 
+import {
+    Bar,
+    BarChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from "recharts";
 import { Helper } from "../Helper";
 import Stat from "./Stat";
+import { useState } from "react";
 
 export function StatSection(props) {
+    const [viewAsGraph, setViewAsGraph] = useState(false);
 
     let hasUndefined = true;
     const statList = Array.from(props.sectionStatNames.keys()).map((stat) => {
         // Only show stats that are known
-        if (props.sectionStatNames.get(stat) !== undefined && props.stats[stat] !== undefined) {
+        if (
+            props.sectionStatNames.get(stat) !== undefined &&
+            props.stats[stat] !== undefined
+        ) {
             hasUndefined = false; // as there is at least one stat in this section
             return (
                 <Stat
@@ -24,6 +37,27 @@ export function StatSection(props) {
         }
     });
 
+    let weaponGraphData = undefined;
+    if (props.sectionName === "Weapon Kills") {
+        weaponGraphData = Array.from(props.sectionStatNames.keys()).map(
+            (stat) => {
+                // Only show stats that are known
+                if (
+                    props.sectionStatNames.get(stat) !== undefined &&
+                    props.stats[stat] !== undefined
+                ) {
+                    hasUndefined = false; // as there is at least one stat in this section
+                    return {
+                        name: stat,
+                        value: props.stats[stat].basic.value,
+                    };
+                } else {
+                    return;
+                }
+            }
+        );
+    }
+
     // Hide section name if there are no known stats for it
     if (hasUndefined) {
         return;
@@ -32,7 +66,25 @@ export function StatSection(props) {
             <div className="stat-section-container">
                 <h3>{props.sectionName}</h3>
                 <hr />
-                <div className="stat-section">{statList}</div>
+                {props.sectionName === "Weapon Kills" && (
+                    <button onClick={() => setViewAsGraph(!viewAsGraph)}>
+                        {viewAsGraph === false
+                            ? "View as Graph"
+                            : "View as Text"}
+                    </button>
+                )}
+                {viewAsGraph === false ? (
+                    <div className="stat-section">{statList}</div>
+                ) : (
+                    <ResponsiveContainer minWidth={400} minHeight={400}>
+                        <BarChart data={weaponGraphData}>
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Bar dataKey="value" fill="#82ca9d" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                )}
             </div>
         );
     }
